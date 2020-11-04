de02a6c590db0ddd337039c8faf070e4c3543b81
Chg <1813784575@qq.com>
2020-11-04 20:54:16 +0800

    11.4

---
+++ app/src/main/java/com/example/my_application1/TitleLayout.java
@@ -0,0 +1,17 @@
+package com.example.my_application1;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+/*
+public class TitleLayout extends LinearLayout {
+
+    @SuppressLint("ResourceType")
+    public TitleLayout(Context context, AttributeSet attrs){
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.menu.title, this);
+    }
+}
+*/
