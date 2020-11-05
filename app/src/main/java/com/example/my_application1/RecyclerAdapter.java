9929eb94b19883854460129c06260ffb07562b47
Github72000 <70786086+Github72000@users.noreply.github.com>
2020-11-05 21:31:01 +0800

    v1.1

--- app/src/main/java/com/example/my_application1/RecyclerAdapter.java
+++ app/src/main/java/com/example/my_application1/RecyclerAdapter.java
@@ -1,11 +1,62 @@
 package com.example.my_application1;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
-/*
-public class RecyclerAdapter extends RecylerView.Adapter<RecyclerAdapter.ViewHolder>{
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
     private List<Fruit> mFruitList;
 
 
 
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view){
+            super(view);
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+
+    }
+
+    public RecyclerAdapter(List<Fruit> fruitList){
+        mFruitList=fruitList;
+    }
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fruit_item,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return mFruitList.size();
+    }
 }
-*/
