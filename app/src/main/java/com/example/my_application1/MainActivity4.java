75e8db9f8a1990097c66b0c95a266fc082a15351
Chg <1813784575@qq.com>
2020-11-07 17:19:06 +0800

    11.7

--- app/src/main/java/com/example/my_application1/MainActivity4.java
+++ app/src/main/java/com/example/my_application1/MainActivity4.java
@@ -1,15 +1,20 @@
 package com.example.my_application1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -21,10 +26,17 @@ public class MainActivity4 extends AppCompatActivity {
         setContentView(R.layout.activity_main4);
 
         initFruits();
-        FruitAdapter adapter = new FruitAdapter(MainActivity4.this,R.layout.fruit_item,fruitList);
-/*
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-        MainActivity3.this,android.R.layout.simple_expandable_list_item_1,data);*/
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        StaggeredGridLayoutManager layoutManager = new
+                StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerAdapter adapter = new RecyclerAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+      //  ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+      //  MainActivity3.this,android.R.layout.simple_expandable_list_item_1,data);
+        /*
         ListView listView = (ListView) findViewById(R.id.list_view);
 
         listView.setAdapter(adapter);
@@ -35,18 +47,30 @@ public class MainActivity4 extends AppCompatActivity {
                 Toast.makeText(MainActivity4.this,fruit.getName(),Toast.LENGTH_SHORT).show();
             }
         });
-
+*/
 
     }
 
     private void initFruits() {
         for(int i=0;i<2;i++)
         {
-            Fruit apple = new Fruit("apple",R.drawable.ic_launcher_background);
+            Fruit apple = new Fruit(getRandomLengthName("apple"),R.drawable.ic_launcher_background);
             fruitList.add(apple);
-            Fruit banana = new Fruit("banana",R.drawable.ic_launcher_foreground);
+            Fruit banana = new Fruit(getRandomLengthName("banana"),R.drawable.ic_launcher_foreground);
             fruitList.add(banana);
+            Fruit tomato = new Fruit(getRandomLengthName("tomato"),R.drawable.ic_launcher_background);
+            fruitList.add(tomato);
         }
     }
 
+    private String getRandomLengthName(String name)
+    {
+        Random random = new Random();
+        int length = random.nextInt(20)+1;
+        StringBuilder builder = new StringBuilder();
+        for(int i=0 ; i<length ; i++){
+            builder.append(name);
+        }
+        return builder.toString();
+    }
 }
--- app/src/main/java/com/example/my_application1/RecyclerAdapter.java
+++ app/src/main/java/com/example/my_application1/RecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,9 +22,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView fruitImage;
         TextView fruitName;
+        View fruitView;
 
         public ViewHolder(View view){
             super(view);
+            fruitView = view;
             fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
             fruitName = (TextView) view.findViewById(R.id.fruit_name);
         }
@@ -37,17 +40,38 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
 
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fruit_item,parent,false);
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.fruitView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                    int position = holder.getAdapterPosition();
+                    Fruit fruit = mFruitList.get(position);
+                    Toast.makeText(v.getContext(),"you clicked view "+fruit.getName(),
+                            Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        holder.fruitImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(v.getContext(),"you clicked image "+fruit.getName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         return holder;
+
 
 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
         Fruit fruit = mFruitList.get(position);
         holder.fruitImage.setImageResource(fruit.getImageId());
